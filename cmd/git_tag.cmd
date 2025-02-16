268bcd2f798570037f3cdf2f2554a029e00444fb
liledix4 <liledix4@musician.org>
2025-02-16 12:58:56 +0000

    0.1.2

--- cmd/git_tag.cmd
+++ cmd/git_tag.cmd
@@ -8,7 +8,8 @@ call :start
   goto :dir
 :dir
   set /p "_dir=Enter directory: "
-  if exist "%_dir%\.git" (cd %_dir% && goto :commit) else (goto :dir)
+  if not defined _dir (if exist ".git" (goto :commit) else (echo Incorrect directory.&& goto :dir))
+  if exist "%_dir%\.git" (cd %_dir% && goto :commit) else (echo Incorrect directory.&& goto :dir)
 :commit
   set /p "_commit=Enter commit ID (either 7 or 40 symbols long): "
   for /f %%i in ('git cat-file commit %_commit%') do (set _commitfound=%%i)
